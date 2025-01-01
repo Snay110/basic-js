8366295f07d9c08c16ebcedd503cec77c2e69263
Oleksandr Murga <sanay8myrga@gmail.com>
2025-01-01 16:03:27 +0200

    feat: решил задачу с вычислением возраста находки (carbon-dating)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample( sampleActivity ) {
+  if (
+    typeof sampleActivity !== 'string' ||
+    isNaN(parseFloat(sampleActivity)) ||
+    parseFloat(sampleActivity) <= 0 ||
+    parseFloat(sampleActivity) > 15
+  ) {
+    return false;
+  }
+
+  sampleActivity = parseFloat(sampleActivity);
+  const k = Math.log(2) / 5730;
+  return Math.ceil(Math.log(15 / sampleActivity) / k);
 }
 
 module.exports = {
